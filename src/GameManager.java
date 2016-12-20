67c69e655703e78045134ec4a68c216b0b1d9422
amrshosny <amrshosny@gmail.com>
2016-12-20 20:18:41 +0200

    Add files via upload

---
+++ src/GameManager.java
@@ -0,0 +1,36 @@
+
+public class GameManager 
+{
+	GameModel gameModel;
+	int latestEarnedScore;
+	
+	GameManager (GameModel gameModel )
+	{
+		this.gameModel = gameModel;
+	}
+	
+	void validateFormContent ( GameForm gameForm )
+	{
+		
+	}
+	
+	void runGameInterface ( String gameName )
+	{
+		Game game = gameModel.retrieveGame(gameName);
+		GameInterface gameInterface = new GameInterface(game);
+		
+		// Will be running until the user exits or finishes the game
+		
+		gameInterface.run();
+		
+		latestEarnedScore = game.getPlayerScore();
+		System.out.println(latestEarnedScore);		
+	}
+	
+	void addLatestEarnedScoreToAccount ( AccountManager accountManager , String username )
+	{
+		accountManager.addScore(username, latestEarnedScore);
+	}
+	
+	
+}
